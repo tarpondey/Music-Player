d21e690e10c00da4ec4064e985ab5ce4b9649790
Tarpon Dey <61899753+tarpondey@users.noreply.github.com>
2022-09-04 00:19:45 +0530

    Add files via upload

---
+++ music_player/assets/data.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+      name: 'Ae Watan',
+      path: './assets/audio/1.mp3',
+      artist: 'Arijit Singh',
+      cover: 'assets/cover/download.jpg'
+    },
+  
+    {
+      name: 'Aye Mere Watan Ke Logo',
+      path: './assets/audio/2.mp3',
+      artist: 'Lata Mangeshkar',
+      cover: 'assets/cover/mere_watan.jpg'
+    },
+  
+    {
+      name: 'Sandeshe Aate Hai',
+      path: 'assets/audio/3.mp3',
+      artist: 'Sonu Nigam',
+      cover: 'assets/cover/sandese_aati.jpg'
+    },
+  
+    {
+      name: 'Saare Jahan Se Acchha',
+      path: 'assets/audio/4.mp3',
+      artist: 'Bickram Ghosh',
+      cover: 'assets/cover/sare_jahan.jpg'
+    },
+    
+    {
+        name: 'Shape Of You',
+        path: 'assets/audio/6.mp3',
+        artist: 'Ed Sheeran',
+        cover: 'assets/cover/shape_of_you.jpg'
+    },
+
+    {
+        name: 'Aalbida',
+        path: 'assets/audio/7.mp3',
+        artist: 'KK',
+        cover: 'assets/cover/alvida.jpg'
+    },
+
+    {
+      name: 'Aaguner Poroshmoni',
+      path: 'assets/audio/5.mp3',
+      artist: 'Indrani Sen',
+      cover: 'assets/cover/aaguner_parashmani.jpg'
+    }
+  ]
